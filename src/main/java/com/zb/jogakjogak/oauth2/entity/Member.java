3708f2e9b60cf8d4fabdfd12d88b6b7e9af9dabf
DESKTOP-U6CF5AQ\user <min318777@naver.com>
2025-06-02 10:45:36 +0900

    #1 feat oauth2: Member엔티티 생성

---
+++ src/main/java/com/zb/jogakjogak/oauth2/entity/Member.java
@@ -0,0 +1,37 @@
+package com.zb.jogakjogak.oauth2.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String phoneNumber;
+}
