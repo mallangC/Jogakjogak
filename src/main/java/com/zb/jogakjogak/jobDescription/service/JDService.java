b0c1b8235449ed8bd1dd4ec6bedf066f04e51087
mingang211 <a989977@naver.com>
2025-06-02 16:51:44 +0900

    #2 feat-feature/JD: JDService 추가 <본문> - 이력서, JD는 하드 코딩 상태 나중에 이력서 추가기능, 생기면 수정 예정

---
+++ src/main/java/com/zb/jogakjogak/jobDescription/service/JDService.java
@@ -0,0 +1,28 @@
+package com.zb.jogakjogak.jobDescription.service;
+
+import com.zb.jogakjogak.jobDescription.domain.requestDto.JDRequestDto;
+import com.zb.jogakjogak.jobDescription.domain.responseDto.JDResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class JDService {
+
+    private final OpenAIResponseService openAIResponseService;
+
+    public JDResponseDto analyze (JDRequestDto jdRequestDto) {
+        String resume = "5년차 백엔드 개발 경험. Java, Spring Boot 능숙. RESTful API 설계 및 개발 경험 다수. MSA 환경 경험";
+        String jobDescription = "백엔드 개발자 채용. Java/Spring Boot 필수. RESTful API 개발 경험 우대. MSA 경험 우대. 적극적인 문제 해결 능력.";
+        String result = openAIResponseService.sendRequest(resume, jobDescription, 0);
+        return JDResponseDto.builder()
+                .title(jdRequestDto.getTitle())
+                .jdUrl(jdRequestDto.getJDUrl())
+                .endedAt(jdRequestDto.getEndedAt())
+                .analysisResult(result)
+                .memo("")
+                .build();
+    }
+}
