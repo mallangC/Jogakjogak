3e3c345ea921249176766da2e904d574f07468c5
mingang211 <a989977@naver.com>
2025-06-02 16:48:33 +0900

    #2 feat-feature/JD: RequestDto 작성 <본문> - 제목의 길이를 255자로 설정, 필수 입력 항목 - URL 형식이어야 하며 필수 입력 항목

---
+++ src/main/java/com/zb/jogakjogak/jobDescription/domain/requestDto/JDRequestDto.java
@@ -0,0 +1,23 @@
+package com.zb.jogakjogak.jobDescription.domain.requestDto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.validator.constraints.URL;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class JDRequestDto {
+    @NotBlank(message = "제목은 필수 입력 항목입니다.")
+    @Size(max = 255, message = "제목의 최대 길이는 255자입니다.")
+    private String title;
+    @NotBlank(message = "채용 공고 URL은 필수 입력 항목입니다.")
+    @URL(message = "유효한 URL 형식이 아닙니다.")
+    private String JDUrl;
+    @NotNull(message = "마감일은 필수 선택 항목입니다.")
+    private LocalDateTime endedAt;
+}
