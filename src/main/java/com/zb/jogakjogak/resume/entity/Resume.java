68f01fc1380dc12ab59c2e9862d449709c3c8c39
mingang211 <a989977@naver.com>
2025-06-04 12:55:59 +0900

    #3 feat-feature/resume: 엔티티 생성 <본문> - BaseEntity를 만들어 entity를 만들 때 extends하여 생성일과 수정일에 대한 중복 코딩작업이 없도록 함. - 이력서 이름은 30자로 제한 - 이력서 내용은 5000자로 제한

---
+++ src/main/java/com/zb/jogakjogak/resume/entity/Resume.java
@@ -0,0 +1,26 @@
+package com.zb.jogakjogak.resume.entity;
+
+import com.zb.jogakjogak.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Resume extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 30)
+    private String name;
+    @Column(nullable = false, length = 5000)
+    private String content;
+    private boolean isBookMark;
+
+}
