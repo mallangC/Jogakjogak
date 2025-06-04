cba2fb9a62f8f6a42f9edffe29fa3398a99d8a48
mingang211 <a989977@naver.com>
2025-06-04 14:25:06 +0900

    #3 docs-feature/resume: javadoc 작성 <본문> - 요청 데이터를 확인하기 위해 controller와 dto 수정 - Contoller 메서드에 @Vaild 추가 - 이력서 이름은 필수 내용이고, content에 5000자 이내로 적도로 유효성 검사

--- src/main/java/com/zb/jogakjogak/resume/controller/ResumeController.java
+++ src/main/java/com/zb/jogakjogak/resume/controller/ResumeController.java
@@ -3,6 +3,7 @@ package com.zb.jogakjogak.resume.controller;
 import com.zb.jogakjogak.global.HttpApiResponse;
 import com.zb.jogakjogak.resume.domain.ResumeRequestDto;
 import com.zb.jogakjogak.resume.service.ResumeService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +19,13 @@ public class ResumeController {
 
     private final ResumeService resumeService;
 
+    /**
+     * 이력서 등록을 위한 컨틀로러 메소드
+     * @param requestDto 이력서 이름, 이력서 내용
+     * @return data(이력서 id, 이력서 이름, 이력서 내용), 성공 여부 메세지, 상태코드
+     */
     @PostMapping
-    public ResponseEntity register(@RequestBody ResumeRequestDto requestDto) {
+    public ResponseEntity register(@Valid @RequestBody ResumeRequestDto requestDto) {
         return ResponseEntity.ok()
                 .body(
                         new HttpApiResponse(
