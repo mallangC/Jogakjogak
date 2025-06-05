98f5299be8e84a30ca1f0ca30fe4ae2c0980abe6
mingang211 <a989977@naver.com>
2025-06-05 20:11:12 +0900

    #2 refactor-feature/jd: 에로코드 추가

--- src/main/java/com/zb/jogakjogak/global/exception/JDErrorCode.java
+++ src/main/java/com/zb/jogakjogak/global/exception/JDErrorCode.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum JDErrorCode {
 
-    FAILED_ANALYSIS_REQUEST(HttpStatus.BAD_REQUEST,"분석에 실패했습니다." +
-            " 확인하고 다시 시도해주세요")
+    FAILED_ANALYSIS_REQUEST(HttpStatus.BAD_REQUEST,"분석에 실패했습니다." + " 확인하고 다시 시도해주세요"),
+    INVALID_API_REQUEST(HttpStatus.BAD_REQUEST, "클라이언트 오류 처리" ),
+    API_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류" ),
+    FAILED_JSON_PROCESS(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 파싱 오류")
     ;
 
     private final HttpStatus httpStatus;
