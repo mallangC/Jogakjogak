8998dcb723e7773023bf1788ad00b1b2ebddb211
DESKTOP-U6CF5AQ\user <min318777@naver.com>
2025-06-05 12:03:53 +0900

    #1 feat-oauth2: MemberRepository 추가

--- src/main/java/com/zb/jogakjogak/security/repository/MemberRepository.java
+++ src/main/java/com/zb/jogakjogak/security/repository/MemberRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findByUserName(String userName);
+    boolean existsByUserName(String userName);
 }
