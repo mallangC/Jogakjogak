6cdc5ce6c4491416d71605c22e178d36619f0d15
mingang211 <a989977@naver.com>
2025-06-09 21:21:24 +0900

    #8 test-feature/jd: test 수정

--- src/test/java/com/zb/jogakjogak/resume/service/ResumeServiceTest.java
+++ src/test/java/com/zb/jogakjogak/resume/service/ResumeServiceTest.java
@@ -21,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.Mockito.*;
 
 
