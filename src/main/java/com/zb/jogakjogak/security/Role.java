0117669cd87db9e5ab3e1cff9447bd50c3e7f418
mingang211 <a989977@naver.com>
2025-06-09 17:42:04 +0900

    #20 chore-feature/jd: build.gradle 수정 OAuth 로그인 방지를 위한 수정

--- src/main/java/com/zb/jogakjogak/security/Role.java
+++
@@ -1,6 +0,0 @@
-package com.zb.jogakjogak.security;
-
-public enum Role {
-
-    USER
-}
--- src/main/java/com/zb/jogakjogak/security/Token.java
+++
@@ -1,5 +0,0 @@
-package com.zb.jogakjogak.security;
-
-public enum Token {
-    ACCESS_TOKEN, REFRESH_TOKEN
-}
