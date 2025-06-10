17acbebaa87cf083ab61a18a898af3738200f0ff
mingang211 <a989977@naver.com>
2025-06-10 23:18:18 +0900

    Revert "#25 refactor-feature/JD: 삭제"
    This reverts commit bbea85c147f1950fdc3915484a1a58dc920bf8e1.

---
+++ src/main/java/com/zb/jogakjogak/security/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.zb.jogakjogak.security.repository;
+
+import com.zb.jogakjogak.security.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByUserName(String userName);
+}
