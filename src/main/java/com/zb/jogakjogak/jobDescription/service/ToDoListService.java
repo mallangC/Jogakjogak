430a0277ebbe80dd9c0bd95e847f82d547531fea
mingang211 <a989977@naver.com>
2025-06-10 11:32:08 +0900

    #20 refactor-feature/jd: ToDoList 저장시 연관관계인 JD도 함께 저장하기 위해 수정

--- src/main/java/com/zb/jogakjogak/jobDescription/service/ToDoListService.java
+++ src/main/java/com/zb/jogakjogak/jobDescription/service/ToDoListService.java
@@ -1,6 +1,7 @@
 package com.zb.jogakjogak.jobDescription.service;
 
 import com.zb.jogakjogak.jobDescription.domain.responseDto.ToDoListDto;
+import com.zb.jogakjogak.jobDescription.entity.JD;
 import com.zb.jogakjogak.jobDescription.entity.ToDoList;
 import com.zb.jogakjogak.jobDescription.repsitory.ToDoListRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +16,9 @@ public class ToDoListService {
 
     private final ToDoListRepository toDoListRepository;
 
-    public List<ToDoList> saveToDoList(List<ToDoListDto> toDoList) {
+    public List<ToDoList> saveToDoList(List<ToDoListDto> toDoList, JD jd) {
         List<ToDoList> entities = toDoList.stream()
-                .map(ToDoList::fromDto)
+                .map(dto -> ToDoList.fromDto(dto, jd))
                 .collect(Collectors.toList());
 
         return toDoListRepository.saveAll(entities);
