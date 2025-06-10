18f136de71ca9b9dfe5664940cd066e1cabddb66
mingang211 <a989977@naver.com>
2025-06-10 11:27:37 +0900

    #20 feat-feature/jd: Jd를 저장하기 위한 레포지토리 추가

---
+++ src/main/java/com/zb/jogakjogak/jobDescription/repsitory/JDRepository.java
@@ -0,0 +1,10 @@
+package com.zb.jogakjogak.jobDescription.repsitory;
+
+import com.zb.jogakjogak.jobDescription.entity.JD;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JDRepository extends JpaRepository<JD, Long> {
+
+}
