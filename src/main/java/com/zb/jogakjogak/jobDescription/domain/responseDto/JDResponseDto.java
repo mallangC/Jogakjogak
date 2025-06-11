5ba05a209967657857434d295155035dd2f6560f
mingang211 <a989977@naver.com>
2025-06-12 07:49:22 +0900

    #39 refactor-feature/JD분석내용단건조회: JDResponseDto 수정

--- src/main/java/com/zb/jogakjogak/jobDescription/domain/responseDto/JDResponseDto.java
+++ src/main/java/com/zb/jogakjogak/jobDescription/domain/responseDto/JDResponseDto.java
@@ -1,22 +1,59 @@
 package com.zb.jogakjogak.jobDescription.domain.responseDto;
 
-import com.zb.jogakjogak.jobDescription.domain.requestDto.ToDoListDto;
+import com.zb.jogakjogak.jobDescription.entity.JD;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.Hibernate;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
 public class JDResponseDto {
+    private Long jd_id;
     private String title;
+    private String companyName;
+    private String job;
+    private String content;
     private String jdUrl;
-    private List<ToDoListDto> analysisResult;
     private String memo;
+    private boolean isAlarmOn;
+    private LocalDateTime applyAt;
     private LocalDate endedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private List<ToDoListResponseDto> toDoLists;
+
+    public static JDResponseDto fromEntity(JD jd) {
+        List<ToDoListResponseDto> mappedToDoLists = null;
+        if (jd.getToDoLists() != null && Hibernate.isInitialized(jd.getToDoLists())) {
+            mappedToDoLists = jd.getToDoLists().stream()
+                    .map(ToDoListResponseDto::fromEntity)
+                    .collect(Collectors.toList());
+        }
+
+        return JDResponseDto.builder()
+                .jd_id(jd.getId())
+                .title(jd.getTitle())
+                .companyName(jd.getCompanyName())
+                .job(jd.getJob())
+                .content(jd.getContent())
+                .jdUrl(jd.getJdUrl())
+                .memo(jd.getMemo())
+                .isAlarmOn(jd.isAlarmOn())
+                .applyAt(jd.getApplyAt())
+                .endedAt(jd.getEndedAt())
+                .createdAt(jd.getCreatedAt())
+                .updatedAt(jd.getUpdatedAt())
+                .toDoLists(mappedToDoLists)
+                .build();
+    }
 }
