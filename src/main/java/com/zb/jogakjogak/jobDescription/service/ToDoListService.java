c6d9c77e0cee3e8d2f9d3c328709bf90808dbb16
mingang211 <a989977@naver.com>
2025-06-12 02:43:12 +0900

    #24 refactor-feature/ToDoList-삭제: 메서드 추가 - 반복되는 코드를 메서드로 바꾸어 중복 코드를 제거하고 가독성향상, 재사용성을 높였습니다.

--- src/main/java/com/zb/jogakjogak/jobDescription/service/ToDoListService.java
+++ src/main/java/com/zb/jogakjogak/jobDescription/service/ToDoListService.java
@@ -32,9 +32,7 @@ public class ToDoListService {
     @Transactional
     public ToDoListResponseDto createToDoList(Long jdId, ToDoListDto toDoListDto) {
 
-        JD jd = jdRepository.findById(jdId)
-                .orElseThrow(() -> new JDException(JDErrorCode.JD_NOT_FOUND));
-
+        JD jd = findJdById(jdId);
         ToDoList toDoList = ToDoList.fromDto(toDoListDto, jd);
         jd.addToDoList(toDoList);
         ToDoList savedToDoList = toDoListRepository.save(toDoList);
@@ -51,16 +49,7 @@ public class ToDoListService {
      */
     @Transactional
     public ToDoListResponseDto updateToDoList(Long jdId, Long toDoListId, ToDoListDto toDoListDto) {
-        jdRepository.findById(jdId)
-                .orElseThrow(() -> new JDException(JDErrorCode.JD_NOT_FOUND));
-
-        ToDoList toDoList = toDoListRepository.findById(toDoListId)
-                .orElseThrow(() -> new ToDoListException(ToDoListErrorCode.TODO_LIST_NOT_FOUND));
-
-        if (!toDoList.getJd().getId().equals(jdId)) {
-            throw new ToDoListException(ToDoListErrorCode.TODO_LIST_NOT_BELONG_TO_JD);
-        }
-
+        ToDoList toDoList = findToDoListAndValidateOwnership(jdId, toDoListId);
         toDoList.updateFromDto(toDoListDto);
         ToDoList updatedToDoList = toDoListRepository.save(toDoList);
         return ToDoListResponseDto.fromEntity(updatedToDoList);
@@ -74,15 +63,7 @@ public class ToDoListService {
      * @return 조회된 ToDoList의 응답 DTO
      */
     public ToDoListResponseDto getToDoList(Long jdId, Long toDoListId) {
-        jdRepository.findById(jdId)
-                .orElseThrow(() -> new JDException(JDErrorCode.JD_NOT_FOUND));
-
-        ToDoList toDoList = toDoListRepository.findById(toDoListId)
-                .orElseThrow(() -> new ToDoListException(ToDoListErrorCode.TODO_LIST_NOT_FOUND));
-
-        if (!toDoList.getJd().getId().equals(jdId)) {
-            throw new ToDoListException(ToDoListErrorCode.TODO_LIST_NOT_BELONG_TO_JD);
-        }
+        ToDoList toDoList = findToDoListAndValidateOwnership(jdId, toDoListId);
         return ToDoListResponseDto.fromEntity(toDoList);
     }
 
@@ -95,8 +76,36 @@ public class ToDoListService {
      */
     public ToDoListDeleteResponseDto deleteToDoList(Long jdId, Long toDoListId) {
 
-        jdRepository.findById(jdId)
+        ToDoList toDoList = findToDoListAndValidateOwnership(jdId, toDoListId);
+
+        toDoListRepository.delete(toDoList);
+        return ToDoListDeleteResponseDto.builder()
+                .checklist_id(toDoList.getId())
+                .build();
+    }
+
+    /**
+     * ID로 JD를 찾고, 없으면 예외를 발생시킵니다.
+     * Optional을 사용하여 null 체크 대신 존재 여부를 명확히 처리합니다.
+     * @param jdId 찾을 JD의 ID
+     * @return 찾아진 JD 엔티티
+     * @throws JDException JD를 찾을 수 없을 경우
+     */
+    private JD findJdById(Long jdId) {
+        return jdRepository.findById(jdId)
                 .orElseThrow(() -> new JDException(JDErrorCode.JD_NOT_FOUND));
+    }
+
+    /**
+     * ToDoList를 ID로 찾고, 해당 JD에 속하는지 검증합니다.
+     * Optional 체이닝을 활용하여 가독성을 높이고 null 체크를 줄입니다.
+     * @param jdId 검증할 JD의 ID
+     * @param toDoListId 찾을 ToDoList의 ID
+     * @return 찾아진 ToDoList 엔티티
+     * @throws ToDoListException ToDoList를 찾을 수 없거나 해당 JD에 속하지 않을 경우
+     */
+    private ToDoList findToDoListAndValidateOwnership(Long jdId, Long toDoListId) {
+        findJdById(jdId);
 
         ToDoList toDoList = toDoListRepository.findById(toDoListId)
                 .orElseThrow(() -> new ToDoListException(ToDoListErrorCode.TODO_LIST_NOT_FOUND));
@@ -104,10 +113,6 @@ public class ToDoListService {
         if (!toDoList.getJd().getId().equals(jdId)) {
             throw new ToDoListException(ToDoListErrorCode.TODO_LIST_NOT_BELONG_TO_JD);
         }
-
-        toDoListRepository.delete(toDoList);
-        return ToDoListDeleteResponseDto.builder()
-                .checklist_id(toDoList.getId())
-                .build();
+        return toDoList;
     }
 }
