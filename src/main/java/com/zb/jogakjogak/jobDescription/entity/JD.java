b08d472a16c365d054437664eb0484b57caecf75
mingang211 <a989977@naver.com>
2025-06-12 06:18:48 +0900

    #39 refactor-feature/JD분석내용단건조회: JD 엔티티 칼럼 추가 - company(회사 이름), job(직무 이름), content(채용 공고), isAlarmOn(알람 on/off), applyAt(지원완료일) 추가 - endedAt을 localDate로 변경

--- src/main/java/com/zb/jogakjogak/jobDescription/entity/JD.java
+++ src/main/java/com/zb/jogakjogak/jobDescription/entity/JD.java
@@ -1,11 +1,13 @@
 package com.zb.jogakjogak.jobDescription.entity;
 
+import com.zb.jogakjogak.global.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,14 +18,23 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class JD {
+public class JD extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false)
     private String title;
+
+    @Column
+    private String companyName;
+
+    @Column
+    private String job;
+
+    @Column
+    private String content;
 
     @Column(nullable = true)
     private String jdUrl;
@@ -31,8 +42,14 @@ public class JD {
     @Column(nullable = true)
     private String memo;
 
+    @Column
+    private boolean isAlarmOn;
+
+    @Column
+    private LocalDateTime applyAt;
+
     @Column(nullable = false)
-    private LocalDateTime endedAt;
+    private LocalDate endedAt;
     
     @Builder.Default
     @OneToMany(mappedBy = "jd", cascade = CascadeType.ALL, orphanRemoval = true)
