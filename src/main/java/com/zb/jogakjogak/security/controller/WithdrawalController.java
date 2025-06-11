1b9638339b24e5834a850e47d4d2b6007c8ca4c9
DESKTOP-U6CF5AQ\user <min318777@naver.com>
2025-06-11 13:58:10 +0900

    #32 feat-feat/withdrawal: WithdrawalController 회원탈퇴 컨트롤러 추가

---
+++ src/main/java/com/zb/jogakjogak/security/controller/WithdrawalController.java
@@ -0,0 +1,55 @@
+package com.zb.jogakjogak.security.controller;
+
+
+import com.zb.jogakjogak.global.HttpApiResponse;
+import com.zb.jogakjogak.security.dto.CustomOAuth2User;
+import com.zb.jogakjogak.security.service.WithdrawalService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member/withdrawal")
+public class WithdrawalController {
+
+    private final WithdrawalService withdrawalService;
+
+    @PostMapping()
+    public HttpApiResponse<?> oauth2Withdrawal(HttpServletResponse response){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()){
+            return  new HttpApiResponse<>(null, "회원탈퇴요청 실패", HttpStatus.UNAUTHORIZED);
+        }
+        String userName = extractUsername(authentication);
+        withdrawalService.withdrawMember(userName);
+        clearRefreshTokenCookie(response);
+        SecurityContextHolder.clearContext();
+        return new HttpApiResponse<>(null, "회원탈퇴 완료", HttpStatus.OK);
+    }
+
+    private String extractUsername(Authentication authentication) {
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof CustomOAuth2User) {
+            CustomOAuth2User oAuth2User = (CustomOAuth2User) principal;
+            return oAuth2User.getName();
+        }
+        return principal.toString();
+    }
+
+    private void clearRefreshTokenCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("refresh", null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
+    }
+}
