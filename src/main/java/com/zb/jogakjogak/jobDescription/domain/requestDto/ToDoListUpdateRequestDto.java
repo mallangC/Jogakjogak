ebeb9694c101cae65bb1c542393a17be35001f51
mingang211 <a989977@naver.com>
2025-06-13 19:22:32 +0900

    #47 refactor-feature/ToDOList카테고리별조회: @Builder 추가

--- src/main/java/com/zb/jogakjogak/jobDescription/domain/requestDto/ToDoListUpdateRequestDto.java
+++ src/main/java/com/zb/jogakjogak/jobDescription/domain/requestDto/ToDoListUpdateRequestDto.java
@@ -2,9 +2,11 @@ package com.zb.jogakjogak.jobDescription.domain.requestDto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.zb.jogakjogak.jobDescription.type.ToDoListType;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ToDoListUpdateRequestDto {
     @JsonProperty("checklist_id")
     private Long id;
