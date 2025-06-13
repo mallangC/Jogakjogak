844f7b51d211acc5fbbe94864e36dbb9cb6e41b6
mingang211 <a989977@naver.com>
2025-06-13 17:19:58 +0900

    #47 feat-feature/ToDOList카테고리별조회: Jd와 카테고리 todolist를 찾는 메서드 추가

--- src/main/java/com/zb/jogakjogak/jobDescription/repsitory/ToDoListRepository.java
+++ src/main/java/com/zb/jogakjogak/jobDescription/repsitory/ToDoListRepository.java
@@ -1,10 +1,15 @@
 package com.zb.jogakjogak.jobDescription.repsitory;
 
+import com.zb.jogakjogak.jobDescription.entity.JD;
 import com.zb.jogakjogak.jobDescription.entity.ToDoList;
+import com.zb.jogakjogak.jobDescription.type.ToDoListType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ToDoListRepository extends JpaRepository<ToDoList, Long> {
 
+    List<ToDoList> findByJdAndCategory(JD jd, ToDoListType targetCategory);
 }
