a4afeba93bf73780228f3ee50dac4b9a98907e9e
mingang211 <a989977@naver.com>
2025-06-16 17:12:51 +0900

    #40 feat-feature/JD분석내용전체조회: 여러개에 jd를 받을 responseDto 생성

---
+++ src/main/java/com/zb/jogakjogak/jobDescription/domain/responseDto/AllGetJDResponseDto.java
@@ -0,0 +1,25 @@
+package com.zb.jogakjogak.jobDescription.domain.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AllGetJDResponseDto {
+    private Long jd_id;
+    private String title;
+    private String companyName;
+    private Long total_pieces;
+    private Long completed_pieces;
+    private LocalDateTime applyAt;
+    private LocalDate endedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
