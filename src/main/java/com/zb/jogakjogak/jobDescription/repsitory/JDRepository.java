2dba953961eec284ae4e09ad7d62f832451550ff
mingang211 <a989977@naver.com>
2025-06-16 17:17:14 +0900

    #40 feat-feature/JD분석내용전체조회: Paginaion을 하기 위한 메서드 추가

--- src/main/java/com/zb/jogakjogak/jobDescription/repsitory/JDRepository.java
+++ src/main/java/com/zb/jogakjogak/jobDescription/repsitory/JDRepository.java
@@ -1,6 +1,9 @@
 package com.zb.jogakjogak.jobDescription.repsitory;
 
 import com.zb.jogakjogak.jobDescription.entity.JD;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +15,7 @@ import java.util.Optional;
 public interface JDRepository extends JpaRepository<JD, Long> {
     @Query("SELECT jd FROM JD jd JOIN FETCH jd.toDoLists WHERE jd.id = :id")
     Optional<JD> findByIdWithToDoLists(@Param("id") Long id);
+
+    @EntityGraph(attributePaths = "toDoLists")
+    Page<JD> findByMemberId(Long memberId, Pageable pageable);
 }
