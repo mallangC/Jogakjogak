1a75815090fadfbca00733eeab9ea5bd9ee6b251
mingang211 <a989977@naver.com>
2025-06-17 09:19:23 +0900

    #40 refator-feature/JD분석내용전체조회: JD 분석 요청시 제목 30자로 제한

--- src/main/java/com/zb/jogakjogak/jobDescription/domain/requestDto/JDRequestDto.java
+++ src/main/java/com/zb/jogakjogak/jobDescription/domain/requestDto/JDRequestDto.java
@@ -17,7 +17,7 @@ import java.time.LocalDate;
 @Builder
 public class JDRequestDto {
     @NotBlank(message = "제목은 필수 입력 항목입니다.")
-    @Size(max = 255, message = "제목의 최대 길이는 255자입니다.")
+    @Size(max = 30, message = "제목의 최대 길이는 30자입니다.")
     private String title;
     @NotBlank(message = "채용 공고 URL은 필수 입력 항목입니다.")
     @URL(message = "유효한 URL 형식이 아닙니다.")
