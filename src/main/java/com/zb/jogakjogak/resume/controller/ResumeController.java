fbb5f188cc2bc63e9ab77a0e7f2ea443ffcc0cee
mingang211 <a989977@naver.com>
2025-06-17 17:42:49 +0900

    #84 refactor-feature/지원완료즐겨찾기: 컨트롤러 수정 - @AuthenticationPrincipal 추가 - 삭제 메서드에 HttpStatus.NO_CONTENT로 수정 - API 주소 카멜케이스로 수정

--- src/main/java/com/zb/jogakjogak/resume/controller/ResumeController.java
+++ src/main/java/com/zb/jogakjogak/resume/controller/ResumeController.java
@@ -2,7 +2,6 @@ package com.zb.jogakjogak.resume.controller;
 
 import com.zb.jogakjogak.global.HttpApiResponse;
 import com.zb.jogakjogak.resume.domain.requestDto.ResumeRequestDto;
-import com.zb.jogakjogak.resume.domain.responseDto.ResumeDeleteResponseDto;
 import com.zb.jogakjogak.resume.domain.responseDto.ResumeResponseDto;
 import com.zb.jogakjogak.resume.service.ResumeService;
 import com.zb.jogakjogak.security.dto.CustomOAuth2User;
@@ -41,16 +40,20 @@ public class ResumeController {
     }
     /**
      * 이력서 수정을 위한 컨트롤러 메서드
-     * @param resume_id 수정하려는 이력서의 id
+     * @param resumeId 수정하려는 이력서의 id
      * @param requestDto 수정할 이력서 이름, 수정할 이력서 내용
      * @return data(수정한 이력서 id, 수정된 이력서 이름, 수정된 이력서 내용), 성공 여부 메세지, 상태코드
      */
     @PatchMapping("/{resume_id}")
-    public ResponseEntity<HttpApiResponse<ResumeResponseDto>> modify(@PathVariable Long resume_id, @Valid @RequestBody ResumeRequestDto requestDto) {
+    public ResponseEntity<HttpApiResponse<ResumeResponseDto>> modify(
+            @PathVariable Long resumeId,
+            @Valid @RequestBody ResumeRequestDto requestDto,
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+        String username = customOAuth2User.getName();
         return ResponseEntity.ok()
                 .body(
                         new HttpApiResponse<>(
-                                resumeService.modify(resume_id, requestDto),
+                                resumeService.modify(resumeId, requestDto, username),
                                 "이력서 수정 완료",
                                 HttpStatus.OK
                         )
@@ -58,29 +61,36 @@ public class ResumeController {
     }
     /**
      * 사용자가 작성한 이력서를 조회하는 컨트롤러 메서드
-     * @param resume_id 찾으려는 이력서의 id
+     * @param resumeId 찾으려는 이력서의 id
      * @return 찾으려는 이력서의 data, 성공 여부 메세지, 상태코드
      */
-    @GetMapping("/{resume_id}")
-    public ResponseEntity<HttpApiResponse<ResumeResponseDto>> get(@PathVariable Long resume_id) {
+    @GetMapping("/{resumeId}")
+    public ResponseEntity<HttpApiResponse<ResumeResponseDto>> get(
+            @PathVariable Long resumeId,
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+        String username = customOAuth2User.getName();
         return ResponseEntity.ok()
                 .body(
                         new HttpApiResponse<>(
-                                resumeService.get(resume_id),
+                                resumeService.get(resumeId, username),
                                 "이력서 조회 성공",
                                 HttpStatus.OK
                         )
                 );
     }
 
-    @DeleteMapping("/{resume_id}")
-    public ResponseEntity<HttpApiResponse<ResumeDeleteResponseDto>> delete(@PathVariable Long resume_id) {
+    @DeleteMapping("/{resumeId}")
+    public ResponseEntity<HttpApiResponse<String>> delete(
+            @PathVariable Long resumeId,
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+        String username = customOAuth2User.getName();
+        resumeService.delete(resumeId, username);
         return ResponseEntity.ok()
                 .body(
                         new HttpApiResponse<>(
-                                resumeService.delete(resume_id),
+                                "",
                                 "이력서 삭제 성공",
-                                HttpStatus.OK
+                                HttpStatus.NO_CONTENT
                         )
                 );
     }
