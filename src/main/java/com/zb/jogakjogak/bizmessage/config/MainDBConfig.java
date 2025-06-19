5bddc96462bcb80f871024c468a5d5496becb71e
Tod <ckj9001@gmail.com>
2025-06-19 10:35:26 +0900

    infra-fix: application.yml , MainDBConfig 수정 - ddl-auto를 update로 수정 - 네이밍 전략을 스네이크 케이스로 설정

--- src/main/java/com/zb/jogakjogak/bizmessage/config/MainDBConfig.java
+++ src/main/java/com/zb/jogakjogak/bizmessage/config/MainDBConfig.java
@@ -44,7 +44,7 @@ public class MainDBConfig {
         em. setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 
         HashMap<String, Object> properties = new HashMap<>();
-        properties.put("hibernate.hbm2ddl.auto", "create");
+        properties.put("hibernate.hbm2ddl.auto", "update");
         properties.put("hibernate.show_sql", "true");
         properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
         em.setJpaPropertyMap(properties);
