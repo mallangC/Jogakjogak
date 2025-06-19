7ee6bfdfe1815ad369527ff251744edf72efdddf
DESKTOP-U6CF5AQ\user <min318777@naver.com>
2025-06-19 17:47:42 +0900

    #112 feat-feat/oauth2-google: GoogleResponseDto추가

---
+++ src/main/java/com/zb/jogakjogak/security/dto/GoogleResponseDto.java
@@ -0,0 +1,42 @@
+package com.zb.jogakjogak.security.dto;
+
+
+import lombok.AllArgsConstructor;
+
+import java.util.Map;
+
+@AllArgsConstructor
+public class GoogleResponseDto implements OAuth2ResponseDto{
+
+    private final Map<String, Object> attribute;
+
+    @Override
+    public String getProvider() {
+        return "google";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attribute.get("sub").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return attribute.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+        return attribute.get("name").toString();
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return "";
+    }
+
+    @Override
+    public String getNickname(){
+        return "";
+    }
+}
