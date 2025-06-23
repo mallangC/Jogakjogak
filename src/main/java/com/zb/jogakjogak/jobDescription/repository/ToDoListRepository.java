1a5098a66db5091b731881ac4cccc5d396332a9d
mingang211 <a989977@naver.com>
2025-06-23 18:57:55 +0900

    #128 refactor-feature/todolist: lazy 로딩을 해결하기위한 repository 메서드 추가

--- src/main/java/com/zb/jogakjogak/jobDescription/repository/ToDoListRepository.java
+++ src/main/java/com/zb/jogakjogak/jobDescription/repository/ToDoListRepository.java
@@ -5,9 +5,12 @@ import com.zb.jogakjogak.jobDescription.entity.JD;
 import com.zb.jogakjogak.jobDescription.entity.ToDoList;
 import com.zb.jogakjogak.jobDescription.type.ToDoListType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ToDoListRepository extends JpaRepository<ToDoList, Long> {
@@ -15,5 +18,10 @@ public interface ToDoListRepository extends JpaRepository<ToDoList, Long> {
     List<ToDoList> findByJdAndCategory(JD jd, ToDoListType targetCategory);
 
     List<ToDoList> findByJdId(long jdId);
-    Object findByIdAndJdId(Long toDoListId, Long jdId);
+
+    @Query("SELECT t FROM ToDoList t JOIN FETCH t.jd j WHERE j.id = :jdId AND t.category = :category")
+    List<ToDoList> findByJdIdAndCategoryFetch(@Param("jdId") Long jdId, @Param("category") ToDoListType category);
+
+    @Query("SELECT t FROM ToDoList t JOIN FETCH t.jd WHERE t.id = :id")
+    Optional<ToDoList> findByIdWithJd(@Param("id") Long id);
 }
