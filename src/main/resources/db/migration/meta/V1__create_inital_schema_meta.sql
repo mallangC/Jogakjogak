fba8709e15a20c0aca1f133c4c1ef16971ae97a4
DESKTOP-U6CF5AQ\user <min318777@naver.com>
2025-06-26 16:57:38 +0900

    #112 refactor-feat/oauth2-google: meta.sql추가

---
+++ src/main/resources/db/migration/meta/V1__create_inital_schema_meta.sql
@@ -0,0 +1,82 @@
+CREATE TABLE member
+(
+    id            BIGINT PRIMARY KEY AUTO_INCREMENT,
+    username      VARCHAR(50),
+    email         VARCHAR(50) NOT NULL UNIQUE,
+    password      VARCHAR(50),
+    name          VARCHAR(50),
+    nickname      VARCHAR(50),
+    phone_number  VARCHAR(20) NOT NULL,
+    role          VARCHAR(50) NOT NULL,
+    registered_at DATETIME    NOT NULL,
+    last_login_at DATETIME    NOT NULL
+);
+
+CREATE TABLE refresh_token
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    username   VARCHAR(50)  NOT NULL,
+    token      VARCHAR(255) NOT NULL,
+    expiration DATETIME     NOT NULL
+);
+
+CREATE TABLE oauth2_info
+(
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    member_id   BIGINT      NOT NULL,
+    provider    VARCHAR(10) NOT NULL,
+    provider_id VARCHAR(50) NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member (id)
+);
+
+CREATE TABLE resume
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    member_id  BIGINT        NOT NULL,
+    title      VARCHAR(30)   NOT NULL,
+    content    VARCHAR(5000) NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    FOREIGN KEY (member_id) REFERENCES member (id)
+);
+
+CREATE TABLE job_description
+(
+    id           BIGINT PRIMARY KEY AUTO_INCREMENT,
+    member_id    BIGINT        NOT NULL,
+    title        VARCHAR(30)   NOT NULL,
+    company_name VARCHAR(30)   NOT NULL,
+    job          VARCHAR(30)   NOT NULL,
+    content      VARCHAR(4000) NOT NULL,
+    jd_url       VARCHAR(1000) NOT NULL,
+    `memo`       VARCHAR(1000) NOT NULL,
+    is_bookmark  BIT           NOT NULL,
+    is_alarm_on  BIT           NOT NULL,
+    apply_at     DATETIME,
+    ended_at     DATETIME,
+    created_at   DATETIME,
+    updated_at   DATETIME,
+    FOREIGN KEY (member_id) REFERENCES member (id)
+);
+
+CREATE TABLE to_do_list
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    jd_id      BIGINT        NOT NULL,
+    category   VARCHAR(40)   NOT NULL,
+    title      VARCHAR(50)   NOT NULL,
+    content    VARCHAR(1000) NOT NULL,
+    `memo`     VARCHAR(255)  NOT NULL,
+    is_done    BIT           NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    FOREIGN KEY (jd_id) REFERENCES job_description (id)
+);
+
+CREATE TABLE notification
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    member_id  BIGINT NOT NULL,
+    created_at DATETIME,
+    FOREIGN KEY (member_id) REFERENCES member (id)
+);
