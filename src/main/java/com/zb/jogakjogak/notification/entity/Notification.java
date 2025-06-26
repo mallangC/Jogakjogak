253b1dbc38482b822bb15534986ee71d8c3949dc
DESKTOP-U6CF5AQ\user <min318777@naver.com>
2025-06-26 16:14:21 +0900

    #112 refactor-feat/oauth2-google: 알림 엔티티에 jdList를 추가하고 연관관계를 맺도록 수정

--- src/main/java/com/zb/jogakjogak/notification/entity/Notification.java
+++ src/main/java/com/zb/jogakjogak/notification/entity/Notification.java
@@ -4,16 +4,14 @@ package com.zb.jogakjogak.notification.entity;
 import com.zb.jogakjogak.jobDescription.entity.JD;
 import com.zb.jogakjogak.security.entity.Member;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
@@ -25,7 +23,7 @@ public class Notification {
 
     private LocalDateTime createdAt;
 
-    @OneToMany
+    @OneToMany(mappedBy = "notification", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<JD> jdList = new ArrayList<>();
 
     @ManyToOne
