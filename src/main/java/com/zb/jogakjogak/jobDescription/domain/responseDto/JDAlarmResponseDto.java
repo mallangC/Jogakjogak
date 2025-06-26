4b7985e1430db99f5caa8ac3aab91ff11485eefe
Mallangc <cky9001@gmail.com>
2025-06-26 21:23:06 +0900

    infra-document / JD 관련 API문서 작성 - request는 description, example, requiredMode로 문서 작성 - response는 description, example로 문서 작성

--- src/main/java/com/zb/jogakjogak/jobDescription/domain/responseDto/JDAlarmResponseDto.java
+++ src/main/java/com/zb/jogakjogak/jobDescription/domain/responseDto/JDAlarmResponseDto.java
@@ -1,15 +1,18 @@
 package com.zb.jogakjogak.jobDescription.domain.responseDto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+@Schema(description = "분석 알림 설정/미설정 응답 DTO")
 @Getter
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
 public class JDAlarmResponseDto {
+    @Schema(description = "분석 아이디", example = "1")
     private Long jdId;
+    @Schema(description = "분석 알림 설정 여부", example = "true")
     private boolean isAlarmOn;
 }
