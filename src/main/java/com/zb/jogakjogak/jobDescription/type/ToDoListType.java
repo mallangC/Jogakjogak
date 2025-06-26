a4afe0742839ca798cec3444079e73187ba28144
mingang211 <a989977@naver.com>
2025-06-26 17:23:21 +0900

    #146 refactor-feature/JopDescription: prompt 수정 - SCHEDULE_MISC_ERROR("일정 관리 및 기타") 타입명 변경

--- src/main/java/com/zb/jogakjogak/jobDescription/type/ToDoListType.java
+++ src/main/java/com/zb/jogakjogak/jobDescription/type/ToDoListType.java
@@ -9,7 +9,7 @@ public enum ToDoListType {
 
     STRUCTURAL_COMPLEMENT_PLAN("구조적 보완 계획"),
     CONTENT_EMPHASIS_REORGANIZATION_PROPOSAL("내용 강조/재구성 제안(표현 및 피드백 기반)"),
-    EMPLOYMENT_SCHEDULE_RELATED("취업 일정 관련");
+    SCHEDULE_MISC_ERROR("일정 관리 및 기타");
 
     private final String koreanDescription;
 
