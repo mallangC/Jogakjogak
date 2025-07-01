5dc3ef55fc08ac9b1ba9a43fad5dd896790e4411
DESKTOP-U6CF5AQ\user <min318777@naver.com>
2025-07-01 17:40:34 +0900

    #112 refactor-feat/oauth2-google: 이메일 알림 정책 변경으로 JD를 불러오는 조건 변경

--- src/main/java/com/zb/jogakjogak/jobDescription/repository/JDRepository.java
+++ src/main/java/com/zb/jogakjogak/jobDescription/repository/JDRepository.java
@@ -39,8 +39,19 @@ public interface JDRepository extends JpaRepository<JD, Long> {
 
     List<JD> findAllByMember(Member member);
 
-    @Query("SELECT jd FROM JD jd WHERE jd.updatedAt <= :oldDate AND jd.endedAt >= now AND jd.isAlarmOn = true")
-    Page<JD> findNotUpdatedJd(@Param("oldDate")LocalDateTime oldDate, @Param("now") LocalDateTime now, Pageable pageable);
+    @Query("""
+        SELECT jd
+        FROM JD jd
+        WHERE jd.isAlarmOn = true
+          AND jd.endedAt >= :today
+          AND jd.notificationCount < 3
+          AND (jd.lastNotifiedAt IS NULL OR jd.lastNotifiedAt < :todayStart)
+          AND jd.updatedAt <= :threeDaysAgo
+          ORDER BY jd.endedAt ASC
+        """)
+    List<JD> findJdToNotify(@Param("today") LocalDateTime today,
+                            @Param("threeDaysAgo") LocalDateTime threeDaysAgo,
+                            @Param("todayStart") LocalDateTime todayStart);
 
     //테스트용
     @EntityGraph(attributePaths = "member")
