7b8d1b6316c2a7f2f9d906ce8898dc255afe6f47
mingang211 <a989977@naver.com>
2025-07-02 14:49:04 +0900

    #162 feat-feature/todolist: ToDoListRepository 수정 - ToDoListRepositoryCustom을 상속하도록 변경했습니다. - QueryDSL로 대체된 @query 및 메서드 이름 기반 쿼리들(findByJdAndCategory, findByJdIdAndCategoryFetch, findByIdWithJd, countByJdIdAndCategory)은 제거했습니다.

--- src/main/java/com/zb/jogakjogak/jobDescription/repository/ToDoListRepository.java
+++ src/main/java/com/zb/jogakjogak/jobDescription/repository/ToDoListRepository.java
@@ -13,21 +13,10 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ToDoListRepository extends JpaRepository<ToDoList, Long> {
+public interface ToDoListRepository extends JpaRepository<ToDoList, Long>, ToDoListRepositoryCustom {
 
-    List<ToDoList> findByJdAndCategory(JD jd, ToDoListType targetCategory);
-
-    List<ToDoList> findByJdId(Long jdId);
-
+    //테스트용
     List<ToDoList> findAllByJdId(Long jdId);
-
-    @Query("SELECT t FROM ToDoList t JOIN FETCH t.jd j WHERE j.id = :jdId AND t.category = :category")
-    List<ToDoList> findByJdIdAndCategoryFetch(@Param("jdId") Long jdId, @Param("category") ToDoListType category);
-
-    @Query("SELECT t FROM ToDoList t JOIN FETCH t.jd WHERE t.id = :id")
-    Optional<ToDoList> findByIdWithJd(@Param("id") Long id);
-
-    long countByJdIdAndCategory(Long id, ToDoListType targetCategory);
     /*
     @Query("SELECT COUNT(t) FROM toDoList t JOIN t.jd j WHERE t.isDone = true AND j.Id = :jdId")
     Integer countByIsDoneTrueAndJd_JdId(@Param("jdId") Long jdId);
