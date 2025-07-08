752207715384f1b03babfc571bfb0f2348e2600a
mingang211 <a989977@naver.com>
2025-07-08 14:53:36 +0900

    #187 refactor-feature/ga: ErrorResponse를 class에서 record로 수정

--- src/main/java/com/zb/jogakjogak/global/exception/ErrorResponse.java
+++ src/main/java/com/zb/jogakjogak/global/exception/ErrorResponse.java
@@ -2,13 +2,5 @@ package com.zb.jogakjogak.global.exception;
 
 import lombok.Getter;
 
-@Getter
-public class ErrorResponse {
-    private final String errorCode;
-    private final String message;
-
-    public ErrorResponse(String errorCode, String message) {
-        this.errorCode = errorCode;
-        this.message = message;
-    }
+public record ErrorResponse(String errorCode, String message) {
 }
