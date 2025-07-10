582062fb0f483ebd2f901fa893a97d17e139a6c3
mingang211 <a989977@naver.com>
2025-07-10 11:02:55 +0900

    #187 refactor-feature/ga: api 주소 변경으로 인한 수정 - /api/**가 없어져 멤버관련은 /member/** 나버지는 /jds/**로 시작해 해당 경로 요청으로 인터셉트

--- src/main/java/com/zb/jogakjogak/global/config/WebConfig.java
+++ src/main/java/com/zb/jogakjogak/global/config/WebConfig.java
@@ -1,22 +1,22 @@
 package com.zb.jogakjogak.global.config;
 
 import com.zb.jogakjogak.ga.interceptor.GaApiCallInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final GaApiCallInterceptor gaApiCallInterceptor;
 
-    public WebConfig(GaApiCallInterceptor gaApiCallInterceptor) {
-        this.gaApiCallInterceptor = gaApiCallInterceptor;
-    }
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // /api/로 시작하는 모든 요청에 인터셉터 적용
-        registry.addInterceptor(gaApiCallInterceptor).addPathPatterns("/api/**");
+        // '/jds/'로 시작하는 모든 요청에 인터셉터 적용
+        registry.addInterceptor(gaApiCallInterceptor).addPathPatterns("/jds/**");
+        // '/member/'로 시작하는 모든 요청에 인터셉터 적용
+        registry.addInterceptor(gaApiCallInterceptor).addPathPatterns("/member/**");
     }
 }
