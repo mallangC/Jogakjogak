1b93434c38421dc9bea442a20fd164800f86383e
mingang211 <a989977@naver.com>
2025-08-29 18:36:34 +0900

    #268 feat-feature/todolist: todolist 완료 여부 수정 요청 dto 추가

---
+++ src/main/java/com/zb/jogakjogak/jobDescription/domain/requestDto/ToggleTodolistRequestDto.java
@@ -0,0 +1,19 @@
+package com.zb.jogakjogak.jobDescription.domain.requestDto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Schema(description = "투두리스트 완려 상태 요청 DTO")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ToggleTodolistRequestDto {
+    @Schema(description = "todolist 완료 여부", example = "true", requiredMode = Schema.RequiredMode.REQUIRED)
+    @JsonProperty("is_done")
+    private boolean isDone;
+}
