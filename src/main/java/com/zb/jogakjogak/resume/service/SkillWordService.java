5fab5cf6066c344140b018d386aeb00c0de66524
mingang211 <a989977@naver.com>
2025-09-09 16:54:12 +0900

    #279 feat-feature/search: 서비스 추가 -  "C", "R", "Go", "AI", "UX", "UI", "QA", "QC", "PM", "BI", "ML", "DL", "AWS", "GCP", "CAD" 제외 단어 추가 - 영어이고 알파벳 3글자 이하일 때 빈 리스트 반환 - 한국어이고 2글자 이하일 때 빈 리스트 반환

---
+++ src/main/java/com/zb/jogakjogak/resume/service/SkillWordService.java
@@ -0,0 +1,58 @@
+package com.zb.jogakjogak.resume.service;
+
+import com.zb.jogakjogak.resume.entity.SkillWord;
+import com.zb.jogakjogak.resume.repository.SkillWordRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class SkillWordService {
+
+    private final SkillWordRepository skillWordRepository;
+
+    private static final List<String> EXCEPTION_LIST = Arrays.asList(
+            "C", "R", "Go", "AI", "UX", "UI", "QA", "QC",
+            "PM", "BI", "ML", "DL", "AWS", "GCP", "CAD"
+    );
+
+    public List<String> getAutocompleteSuggestions(String query) {
+        if (query == null || query.isEmpty()) {
+            return List.of();
+        }
+
+        if(isEnglish(query) && query.length() < 3 &&
+           !EXCEPTION_LIST.contains(query.toUpperCase())) {
+            return List.of();
+        }
+
+        if(isKorean(query) && query.length() < 2) {
+            return List.of();
+        }
+
+        String fullTextQuery = query + "*";
+
+        List<SkillWord> skills = skillWordRepository.findByContentMatchAgainst(fullTextQuery);
+
+        return skills.stream()
+                .map(SkillWord::getContent)
+                .distinct()
+                .sorted(Comparator.naturalOrder())
+                .limit(8)
+                .collect(Collectors.toList());
+
+    }
+
+    private boolean isEnglish(String query) {
+        return query.matches("^[a-zA-Z\\s]*$");
+    }
+
+    private boolean isKorean(String query) {
+        return query.matches("^[가-힣]*$");
+    }
+}
