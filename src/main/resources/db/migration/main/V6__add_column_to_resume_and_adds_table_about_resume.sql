8c607a121e9af0052bc4653af676a00059ba5bba
Mallangc <cky9001@gmail.com>
2025-09-09 14:14:37 +0900

    resume-update / 새로운 이력서 형식을 위한 sql작성

---
+++ src/main/resources/db/migration/main/V6__add_column_to_resume_and_adds_table_about_resume.sql
@@ -0,0 +1,32 @@
+ALTER TABLE resume
+    ADD COLUMN is_newcomer BIT NOT NULL DEFAULT TRUE;
+
+CREATE TABLE career
+(
+    id               BIGINT PRIMARY KEY AUTO_INCREMENT,
+    resume_id        BIGINT        NOT NULL,
+    joined_at        DATETIME      NOT NULL,
+    quit_at          DATETIME      NOT NULL,
+    is_working       BIT           NOT NULL,
+    company_name     VARCHAR(100)  NOT NULL,
+    work_performance VARCHAR(2000) NOT NULL,
+    FOREIGN KEY (resume_id) REFERENCES resume (id)
+);
+
+CREATE TABLE education
+(
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    resume_id   BIGINT       NOT NULL,
+    level       VARCHAR(20) NOT NULL,
+    major_field DATETIME     NOT NULL,
+    status      VARCHAR(20) NOT NULL,
+    FOREIGN KEY (resume_id) REFERENCES resume (id)
+);
+
+CREATE TABLE skill
+(
+    id        BIGINT PRIMARY KEY AUTO_INCREMENT,
+    resume_id BIGINT       NOT NULL,
+    content   VARCHAR(255) NOT NULL,
+    FOREIGN KEY (resume_id) REFERENCES resume (id)
+);
