dd2cd78d3528d1b830a93f6ff62be550d135b370
Mallangc <cky9001@gmail.com>
2025-09-10 18:53:26 +0900

    resume-update / 이력서 작성 컨트롤러 추가 - ResumeAddRequestDto을 받고 HttpApiResponse<ResumeAddResponse>를 반환

--- src/main/java/com/zb/jogakjogak/resume/controller/ResumeController.java
+++ src/main/java/com/zb/jogakjogak/resume/controller/ResumeController.java
@@ -1,7 +1,9 @@
 package com.zb.jogakjogak.resume.controller;
 
 import com.zb.jogakjogak.global.HttpApiResponse;
+import com.zb.jogakjogak.resume.domain.requestDto.ResumeAddRequestDto;
 import com.zb.jogakjogak.resume.domain.requestDto.ResumeRequestDto;
+import com.zb.jogakjogak.resume.domain.responseDto.ResumeAddResponseDto;
 import com.zb.jogakjogak.resume.domain.responseDto.ResumeResponseDto;
 import com.zb.jogakjogak.resume.service.ResumeService;
 import com.zb.jogakjogak.security.dto.CustomOAuth2User;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 public class ResumeController {
 
     private final ResumeService resumeService;
+
     /**
      * 이력서 등록을 위한 컨틀로러 메소드
      *
@@ -42,9 +45,11 @@ public class ResumeController {
                 )
         );
     }
+
     /**
      * 이력서 수정을 위한 컨트롤러 메서드
-     * @param resumeId 수정하려는 이력서의 id
+     *
+     * @param resumeId   수정하려는 이력서의 id
      * @param requestDto 수정할 이력서 이름, 수정할 이력서 내용
      * @return data(수정한 이력서 id, 수정된 이력서 이름, 수정된 이력서 내용), 성공 여부 메세지, 상태코드
      */
@@ -64,8 +69,10 @@ public class ResumeController {
                         )
                 );
     }
+
     /**
      * 사용자가 작성한 이력서를 조회하는 컨트롤러 메서드
+     *
      * @param resumeId 찾으려는 이력서의 id
      * @return 찾으려는 이력서의 data, 성공 여부 메세지, 상태코드
      */
@@ -100,5 +107,27 @@ public class ResumeController {
                                 HttpStatus.NO_CONTENT
                         )
                 );
+    }
+
+    /**
+     * (v2)이력서 등록을 위한 컨트롤러 메소드
+     *
+     * @param requestDto 이력서 내용, 신입 유무, 경력 리스트, 학력 리스트, 스킬 리스트
+     * @return data(이력서 id, 이력서 내용, 신입 유무, 경력 리스트, 학력 리스트, 스킬 리스트, 생성일시, 수정일시), 성공 여부 메세지, 상태코드
+     */
+    @Operation(summary = "(v2) 이력서 등록", description = "분석할 사용자의 이력서를 등록합니다")
+    @PostMapping("/v2")
+    public ResponseEntity<HttpApiResponse<ResumeAddResponseDto>> registerV2(
+            @Valid @RequestBody ResumeAddRequestDto requestDto,
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+
+        String username = customOAuth2User.getName();
+        return ResponseEntity.ok().body(
+                new HttpApiResponse<>(
+                        resumeService.registerV2(requestDto, username),
+                        "이력서 등록 완료",
+                        HttpStatus.CREATED
+                )
+        );
     }
 }
