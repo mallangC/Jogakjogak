32746ad51709bab70add01d488ac158d82901966
Mallangc <cky9001@gmail.com>
2025-09-30 14:05:14 +0900

    event / 리팩토링

--- src/main/java/com/zb/jogakjogak/event/repository/EventRepository.java
+++ src/main/java/com/zb/jogakjogak/event/repository/EventRepository.java
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long>, EventRepositoryCustom {
-    boolean findByCode(String code);
-
     boolean existsByCode(String code);
 }
--- src/main/java/com/zb/jogakjogak/event/repository/EventRepositoryCustom.java
+++ src/main/java/com/zb/jogakjogak/event/repository/EventRepositoryCustom.java
@@ -2,11 +2,7 @@ package com.zb.jogakjogak.event.repository;
 
 import com.zb.jogakjogak.event.entity.Event;
 import com.zb.jogakjogak.event.type.EventType;
-import com.zb.jogakjogak.security.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface EventRepositoryCustom {
